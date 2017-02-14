a9fd12880228e1c6b28807634b2479485f16baab
Bruno Almeida <bruno@disvolvi.com>
2017-02-13 23:07:40 -0200

    Leap exercise resolution

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,12 +1,11 @@
-// Leap stub file
-
-// The package name is expected by the test program.
 package leap
 
-// testVersion should match the targetTestVersion in the test file.
 const testVersion = 3
 
-// It's good style to write a comment here documenting IsLeapYear.
-func IsLeapYear(int) bool {
-	// Write some code here to pass the test suite.
+func IsLeapYear(year int) bool {
+	if (year%4 == 0 && year%100 != 0) || year%400 == 0 {
+		return true
+	}
+
+	return false
 }
